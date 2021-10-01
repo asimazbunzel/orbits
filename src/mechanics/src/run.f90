dc14d2761394447e711b2307010a59a3c03638e7
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2021-09-30 21:51:34 -0300

    re-write run.f90 for new style

--- src/mechanics/src/run.f90
+++ src/mechanics/src/run.f90
@@ -2,8 +2,8 @@
       program run
 
       use const_def
+      use ctrls_io 
       use mechanics_def
-
       use mechanics_lib
 
       implicit none
@@ -15,13 +15,14 @@
       integer :: ierr
       type(orbit_info), pointer :: o
 
-      logical, parameter :: dbg = .true.
+      logical, parameter :: dbg = .false.
 
       include 'include/formats.inc'
 
       ierr = 0
 
       call system_clock(time0, clock_rate)
+      
 
       id = alloc_orbit(ierr)
       if (ierr /= 0) then
@@ -35,56 +36,25 @@
          return
       end if
 
-      ! now we can set values on orbit_info
-      if (dbg) write(*,'(a)') 'setting init conditions'
-      o% m1 = 1d0 * Msun
-      o% m2 = 1d1 * Msun
-      o% period = 2d0 * (24d0*60d0*60d0)
-      o% separation = &
-         ((standard_cgrav*(o% m1+o% m2))*(o% period/(2*pi))**2)**one_third
-      o% eccentricity = 0d0
-
-      o% initial_dt = 1d-3
-      o% stop_after_n_period = 1d0
-
-      o% implicit_scheme_tolerance = 1d-7
-
-      ! initial position is the periastron
-      if (dbg) write(*,'(a)') 'starting position at periastron'
-      o% x0 = 0d0
-      o% y0 = o% separation * (1 - o% eccentricity)
-      o% vx0 = sqrt((standard_cgrav*o% m2/o% separation) * &
-         (1 + o% eccentricity) / (1 - o% eccentricity))
-      o% vy0 = 0d0
-
-      ! begin loop at init conditions
-      if (dbg) write(*,'(a)') 'begin loop'
-      o% x = o% x0
-      o% y = o% y0
-      o% vx = o% vx0
-      o% vy = o% vy0
-
-      if (dbg) then
-         write(*,'(a)')
-         write(*,1) 'm1', o% m1 / Msun
-         write(*,1) 'm2', o% m2 / Msun
-         write(*,1) 'separation', o% separation / Rsun
-         write(*,1) 'period', o% period / (24d0*60d0*60d0)
-         write(*,1) 'eccentricity', o% eccentricity
-         write(*,1) 'x0, y0', o% x0 / Rsun, o% y0 / Rsun
-         write(*,1) 'xv0, vy0', o% vx0 / 1d5, o% vy0 / 1d5
-         write(*,1) 'x, y', o% x / Rsun, o% y / Rsun
-         write(*,1) 'xv, vy', o% vx / 1d5, o% vy / 1d5
-         write(*,'(a)')
-      end if
-
-
-      call do_one_orbit(id, ierr)
+      call do_one_setup(o, 'orbit_nml', ierr)
       if (ierr /= 0) then
-         write(*,'(a)') 'failed in do_one_orbit'
-         return
+         write(*,'(a)') 'failed in do_one_setup'
+         stop
       end if
+      
+      write(*,'(a)')
+      write(*,1) 'm1', o% m1 / Msun
+      write(*,1) 'm2', o% m2 / Msun
+      write(*,1) 'separation', o% separation / Rsun
+      write(*,1) 'period', o% period / (24d0*60d0*60d0)
+      write(*,1) 'eccentricity', o% eccentricity
+      write(*,'(a)')
 
+      call do_full_orbit(id, ierr)
+      if (ierr /= 0) then
+         write(*,'(a)') 'failed in do_full_orbit'
+         stop
+      end if
 
       call system_clock(time1, clock_rate)
       runtime = real(time1 - time0, dp) / clock_rate / 60
